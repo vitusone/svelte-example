926e66ce545f1f2e1098ef2e739ab0b3c6f0e922
vitusone <93507035+vitusone@users.noreply.github.com>
2021-11-01 14:11:56 +0800

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let count=0
+</script>
+
+<button on:click={()=>count++}>
+	Increment
+</button>
+<button on:click={()=>count--}>
+	Decrement
+</button>
+<button on:click={()=>count=0}>
+	Reset
+</button>
+
+<h2>
+	{count}
+</h2>
